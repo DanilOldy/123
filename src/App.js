23b79c8b9f08adc3d16780c452234049c8fc46c8
Danil <98643049+DanilOldy@users.noreply.github.com>
2022-02-02 14:54:19 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { BrowserRouter, Route, Routes  } from 'react-router-dom';
 import React from 'react';
 import './App.css';
+import Dialogs from './components/Dialog/Dialogs';
 import Header from './components/Header/Header';
 import Nav from './components/Nav/Nav';
 import Profile from './components/Profile/Profile';
 
+
 const App = () => {
   return (
-    <div className='app-wrapper'>
-      <Header />
-      <Nav />
-      <Profile />
-    </div>
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header />
+        <Nav />
+        <div className='app-wrapper-content'>
+          <Routes>
+            <Route path='/dialogs' component={<Dialogs /> } />
+            <Route path='/profile' component={<Profile /> } />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   )
 
 }
